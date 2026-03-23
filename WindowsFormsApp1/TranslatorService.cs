8612e4756363a0fe9f1bb23dcd38f67bd3ca0fe8
RomanPolezhaev <val111111val111@gmail.com>
2026-03-23 20:12:23 +0500

    добавил HTTP-файл

---
+++ WindowsFormsApp1/TranslatorService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+public class TranslationService
+{
+    private readonly string apiKey = "ТВОЙ_API_KEY";
+
+    public async Task<string> TranslateText(string text, string targetLang)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("text", text),
+                new KeyValuePair<string, string>("target_lang", targetLang)
+            });
+
+            client.DefaultRequestHeaders.Add("Authorization", $"DeepL-Auth-Key {apiKey}");
+
+            HttpResponseMessage response = await client.PostAsync(
+                "https://api-free.deepl.com/v2/translate",
+                content
+            );
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
