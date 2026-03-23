f555f9bf402663e51eb1ae9834c707a4b8fc3076
Aman <workemailataniyazov.a@gmail.com>
2026-03-23 21:16:13 +0500

    обновил файл TranslatorApp.cs исправил ошибку конструктура

--- WindowsFormsApp1/TranslatorApp.cs
+++ WindowsFormsApp1/TranslatorApp.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class TranslatorApp : Form
     {
-        public Form1()
+        public TranslatorApp()
         {
             InitializeComponent();
         }
