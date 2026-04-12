d33a393507c47ffd8174944068357aa506bfa5ba
Aman <workemailataniyazov.a@gmail.com>
2026-04-12 13:18:32 +0500

    Изменение в проекте fix убрал с Program.cs тестовую запись при запуске проекта. Было написана для проверки теста БД

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -22,30 +22,6 @@ namespace WindowsFormsApp1
             string connectionString = ConfigurationManager.ConnectionStrings["TranslatorAppDb"].ConnectionString;
             DatabaseInitializer.Initialize(connectionString);
 
-            using (var connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                // Вставляется запись - (id автоинкремент автоматически ставится первичный ключ)
-                string insertSql = "INSERT INTO users (username) VALUES (@username)";
-                using (var cmd = new SQLiteCommand(insertSql, connection))
-                {
-                    cmd.Parameters.AddWithValue("@username", "Тест");
-                    cmd.ExecuteNonQuery();
-                }
-
-                // Чтение всех записей из таблицы users пример
-                string selectSql = "SELECT Id, Username FROM users";
-                using (var cmd = new SQLiteCommand(selectSql, connection))
-                using (SQLiteDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader["id"]}: {reader["username"]}");
-                    }
-                }
-            }
-
             // Запуск формы приложения с обработчиком ошибок
             try
             {
